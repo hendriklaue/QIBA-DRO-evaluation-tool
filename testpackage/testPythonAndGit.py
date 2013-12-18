bdc1fbda4ca582f904c6496cd2d33486348e8caf
Hendrik Laue <hlaue@fastmail.fm>
2013-12-18 12:08:13 +0100

    my first git and python test

---
+++ testpackage/testPythonAndGit.py
@@ -0,0 +1,15 @@
+'''
+My first python program made with eclipse
+Created on Dec 18, 2013
+
+@author: Hendrik Laue
+'''
+
+def sayHello():
+    print "Hello everybody!"
+
+def sayHelloTo(whom):
+    print "Hello "+whom
+    
+sayHello()
+sayHelloTo("Hendrik")
