093a838b4799c07c96f90b8669b94a36632f5681
hlaue <hendrik.laue@mevis.fraunhofer.de>
2014-08-20 13:36:50 +0200

    -corrected name for msall splash image:

--- QIBA_evaluate_tool.py
+++ QIBA_evaluate_tool.py
@@ -897,7 +897,7 @@ class MySplashScreen(wx.SplashScreen):
     def  __init__(self, parent=None):
         # This is a recipe to a the screen.
         # Modify the following variables as necessary.
-        aBitmap = wx.Image(name = "splashImage_small.jpg").ConvertToBitmap()
+        aBitmap = wx.Image(name = "splashImage_smaller.jpg").ConvertToBitmap()
         splashStyle = wx.SPLASH_CENTRE_ON_SCREEN | wx.SPLASH_TIMEOUT
         splashDuration = 2000 # milliseconds
         # Call the constructor with the above arguments in exactly the
